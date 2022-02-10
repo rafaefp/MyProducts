24307f4e1c6c0ea4ea4648228b792f5d661a6c26
rafaefp <rfpimentel@outlook.com>
2022-02-10 20:35:31 -0300

    Creating generic repositories.

--- src/DevIO.Infra/Data/Migrations/Configuration.cs
+++ src/DevIO.Infra/Data/Migrations/Configuration.cs
@@ -1,7 +1,7 @@
 ﻿using DevIO.Infra.Data.Context;
 using System.Data.Entity.Migrations;
 
-namespace DevIO.Infra.Migrations
+namespace DevIO.Infra.Data.Migrations
 {
     internal sealed class Configuration : DbMigrationsConfiguration<MeuDbContext>
     {
