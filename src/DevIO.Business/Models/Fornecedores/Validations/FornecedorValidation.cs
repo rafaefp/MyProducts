d80415a08b215a652b58e5771490735701c12ca7
rafaefp <rfpimentel@outlook.com>
2022-02-19 21:12:23 -0300

    Creating validation with FluentValidation.

---
+++ src/DevIO.Business/Models/Fornecedores/Validations/FornecedorValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace DevIO.Business.Models.Fornecedores.Validations
+{
+    public class FornecedorValidation : AbstractValidator<Fornecedor>
+    {
+        public FornecedorValidation()
+        {
+            RuleFor(f => f.Nome)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido.")
+                .Length(2,200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
+        }
+    }
+}
