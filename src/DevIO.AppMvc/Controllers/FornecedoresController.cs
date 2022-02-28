a0bba54ee52165b51f3b419ce95b124548b13832
rafaefp <rfpimentel@outlook.com>
2022-02-28 18:57:56 -0300

    Creating the other services.

---
+++ src/DevIO.AppMvc/Controllers/FornecedoresController.cs
@@ -0,0 +1,17 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using DevIO.Business.Models.Fornecedores.Services;
+using DevIO.Infra.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevIO.AppMvc.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        
+    }
+}
---
+++ src/DevIO.Business/Core/Notifications/INotificador.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DevIO.Business.Core.Notifications
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+
+        List<Notificacao> ObterNotificacoes();
+
+        void Handle(Notificacao notificacao);
+    }
+}
