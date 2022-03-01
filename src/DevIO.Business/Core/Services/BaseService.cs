04a440ab5ccef25c8b95ef3c985785fa9702d28a
rafaefp <rfpimentel@outlook.com>
2022-03-01 16:09:46 -0300

    Creating error notifications.

--- src/DevIO.Business/Core/Services/BaseService.cs
+++ src/DevIO.Business/Core/Services/BaseService.cs
@@ -1,5 +1,7 @@
 ﻿using DevIO.Business.Core.Models;
+using DevIO.Business.Core.Notifications;
 using FluentValidation;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +12,35 @@ namespace DevIO.Business.Core.Services
 {
     public abstract class BaseService
     {
+        private readonly INotificador _notificador;
+
+        public BaseService(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
+
         protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
         {
             var validator = validacao.Validate(entidade);
 
-            return validator.IsValid;
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return false;
         }
     }
 }
--- src/DevIO.Business/Models/Fornecedores/Services/FornecedorService.cs
+++ src/DevIO.Business/Models/Fornecedores/Services/FornecedorService.cs
@@ -1,4 +1,5 @@
-﻿using DevIO.Business.Core.Services;
+﻿using DevIO.Business.Core.Notifications;
+using DevIO.Business.Core.Services;
 using DevIO.Business.Models.Fornecedores.Validations;
 using System;
 using System.Linq;
@@ -11,7 +12,9 @@ namespace DevIO.Business.Models.Fornecedores.Services
         private readonly IFornecedorRepository _fornecedorRepository;
         private readonly IEnderecoRepository _enderecoRepository;
 
-        public FornecedorService(IFornecedorRepository fornecedorRepository, IEnderecoRepository enderecoRepository)
+        public FornecedorService(IFornecedorRepository fornecedorRepository,
+                                 IEnderecoRepository enderecoRepository,
+                                 INotificador notificador) : base(notificador)
         {
             _fornecedorRepository = fornecedorRepository;
             _enderecoRepository = enderecoRepository;
@@ -40,7 +43,11 @@ namespace DevIO.Business.Models.Fornecedores.Services
         {
             var fornecedor = await _fornecedorRepository.ObterFornecedorProdutosEndereco(id);
 
-            if (fornecedor.Produtos.Any()) return;
+            if (fornecedor.Produtos.Any()) 
+            {
+                Notificar("O fornecedor possui produtos cadastrados!");
+                return; 
+            }
 
             if (fornecedor.Endereco != null)
             {
@@ -61,7 +68,10 @@ namespace DevIO.Business.Models.Fornecedores.Services
         {
             var fornecedorAtual = await _fornecedorRepository.Buscar(f => f.Documento == fornecedor.Documento && f.Id != fornecedor.Id);
 
-            return fornecedorAtual.Any();
+            if (!fornecedorAtual.Any()) return false;
+
+            Notificar("Já existe um fornecedor com este documento informado.");
+            return true;
         }
 
         public void Dispose()
