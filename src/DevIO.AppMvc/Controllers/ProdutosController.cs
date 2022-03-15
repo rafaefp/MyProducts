924a6e93b887fa0c56e9c84e8175a7b71609651f
rafaefp <rfpimentel@outlook.com>
2022-03-15 19:07:16 -0300

    Scaffolding and implementing automapper.

---
+++ src/DevIO.AppMvc/Controllers/ProdutosController.cs
@@ -0,0 +1,136 @@
+﻿using AutoMapper;
+using DevIO.AppMvc.ViewModels;
+using DevIO.Business.Models.Produtos;
+using DevIO.Business.Models.Produtos.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DevIO.AppMvc.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+
+        public ProdutosController()
+        {
+
+        }
+        
+        public async Task<ActionResult> Index()
+        {
+            return View(_mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterTodos()));
+        }
+                
+        public async Task<ActionResult> Details(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produtoViewModel);
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produtos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProdutoViewModel produtoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+                                
+                return RedirectToAction("Index");
+            }
+
+            return View(produtoViewModel);
+        }
+
+        // GET: Produtos/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProdutoViewModel produtoViewModel = await db.ProdutoViewModels.FindAsync(id);
+            if (produtoViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produtoViewModel);
+        }
+
+        // POST: Produtos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,FornecedorId,Nome,Descricao,Imagem,Valor,DataCadastro,Ativo")] ProdutoViewModel produtoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produtoViewModel).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(produtoViewModel);
+        }
+
+        // GET: Produtos/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProdutoViewModel produtoViewModel = await db.ProdutoViewModels.FindAsync(id);
+            if (produtoViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produtoViewModel);
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            ProdutoViewModel produtoViewModel = await db.ProdutoViewModels.FindAsync(id);
+            db.ProdutoViewModels.Remove(produtoViewModel);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private async Task<ProdutoViewModel> ObterProduto(Guid id)
+        {
+            var produto = _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterProdutoFornecedor(id));
+
+            return produto;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
