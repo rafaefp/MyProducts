c8f5c26382aac41a9aec9fc2505bb49e35d57d84
rafaefp <rfpimentel@outlook.com>
2022-04-25 20:19:56 -0300

    Creating address updating partial views and a modal view to edit address.

--- src/DevIO.AppMvc/Scripts/site.js
+++ src/DevIO.AppMvc/Scripts/site.js
@@ -62,27 +62,27 @@ function BuscaCep() {
     });
 }
 
-//function SetModal() {
+function SetModal() {
 
-//    $(document).ready(function () {
-//        $(function () {
-//            $.ajaxSetup({ cache: false });
+    $(document).ready(function () {
+        $(function () {
+            $.ajaxSetup({ cache: false });
 
-//            $("a[data-modal]").on("click",
-//                function (e) {
-//                    $('#myModalContent').load(this.href,
-//                        function () {
-//                            $('#myModal').modal({
-//                                keyboard: true
-//                            },
-//                                'show');
-//                            bindForm(this);
-//                        });
-//                    return false;
-//                });
-//        });
-//    });
-//}
+            $("a[data-modal]").on("click",
+                function (e) {
+                    $('#myModalContent').load(this.href,
+                        function () {
+                            $('#myModal').modal({
+                                keyboard: true
+                            },
+                                'show');
+                            bindForm(this);
+                        });
+                    return false;
+                });
+        });
+    });
+}
 
 //function bindForm(dialog) {
 //    $('form', dialog).submit(function () {
