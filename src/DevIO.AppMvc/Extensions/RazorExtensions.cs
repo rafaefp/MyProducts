a16750f56e38478dc9e527cb866c99712e76efad
rafaefp <rfpimentel@outlook.com>
2022-05-02 20:51:38 -0300

    Creating razor extensions.

---
+++ src/DevIO.AppMvc/Extensions/RazorExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevIO.AppMvc.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormatarDocumento(this WebViewPage page, int tipoPessoa, string documento)
+        {
+            return tipoPessoa == 1 ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00") :
+                                     Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+
+        public static bool ExibirNaURL(this WebViewPage value, Guid Id)
+        {
+            var urlHelper = new UrlHelper(HttpContext.Current.Request.RequestContext);
+            var urlTarget = urlHelper.Action("Edit", "Fornecedores", new { id = Id });
+            var urlTarget2 = urlHelper.Action("ObterEndereco", "Fornecedores", new { id = Id });
+
+            var urlEmUso = HttpContext.Current.Request.Path;
+
+            return urlTarget == urlEmUso || urlTarget2 == urlEmUso;
+        }
+    }
+}
