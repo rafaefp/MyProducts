ddda26f948c33558ed5a96fb3febc134e7617c93
rafaefp <rfpimentel@outlook.com>
2022-05-13 17:10:05 -0300

    Defining automatic date on EF

--- src/DevIO.Infra/Data/Context/MeuDbContext.cs
+++ src/DevIO.Infra/Data/Context/MeuDbContext.cs
@@ -1,8 +1,12 @@
 ﻿using DevIO.Business.Models.Fornecedores;
 using DevIO.Business.Models.Produtos;
 using DevIO.Infra.Data.Mappings;
+using System;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DevIO.Infra.Data.Context
 {
@@ -33,6 +37,20 @@ namespace DevIO.Infra.Data.Context
             modelBuilder.Configurations.Add(new ProdutoConfig());
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(e => e.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+
+                if (entry.State == EntityState.Modified)
+                    entry.Property("DataCadastro").IsModified = false;
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
