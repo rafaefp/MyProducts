f46f08952dc14c1b3c237a5db01d8979acfa73a9
rafaefp <rfpimentel@outlook.com>
2022-05-19 21:26:03 -0300

    Implementing image upload backend

--- src/DevIO.AppMvc/Controllers/ProdutosController.cs
+++ src/DevIO.AppMvc/Controllers/ProdutosController.cs
@@ -5,7 +5,9 @@ using DevIO.Business.Models.Produtos;
 using DevIO.Business.Models.Produtos.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 
 namespace DevIO.AppMvc.Controllers
@@ -64,14 +66,18 @@ namespace DevIO.AppMvc.Controllers
         {
             produtoViewModel = await PopularFornecedores(produtoViewModel);
 
-            if (ModelState.IsValid)
-            {
-                await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
-                                
-                return RedirectToAction("Index");
-            }
+            if (!ModelState.IsValid) return View(produtoViewModel);
 
-            return View(produtoViewModel);
+            var imgPrefix = Guid.NewGuid() + "_";
+
+            if (!UploadImagem(produtoViewModel.ImagemUpload, imgPrefix))
+                return View(produtoViewModel);
+
+            produtoViewModel.Imagem = imgPrefix + produtoViewModel.ImagemUpload.FileName;
+
+            await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+
+            return RedirectToAction("Index");
         }
 
         [Route("editar-produto/{id:guid}")]
@@ -93,16 +99,34 @@ namespace DevIO.AppMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit(ProdutoViewModel produtoViewModel)
         {
-            if (ModelState.IsValid)
-            {
-                await _produtoService.Atualizar(_mapper.Map<Produto>(produtoViewModel));
+            if (!ModelState.IsValid) return View(produtoViewModel);
 
-                return RedirectToAction("Index");
+            var produtoAtualizado = await ObterProduto(produtoViewModel.Id);
+
+            produtoViewModel.Imagem = produtoAtualizado.Imagem;
+
+            if (produtoViewModel.ImagemUpload != null)
+            {
+                var imgPrefixo = Guid.NewGuid() + "_";
+
+                if (!UploadImagem(produtoViewModel.ImagemUpload, imgPrefixo))
+                    return View(produtoViewModel);
+
+                produtoAtualizado.Imagem = imgPrefixo + produtoViewModel.ImagemUpload.FileName;
             }
 
-            return View(produtoViewModel);
+            produtoAtualizado.Nome = produtoViewModel.Nome;
+            produtoAtualizado.Descricao = produtoViewModel.Descricao;
+            produtoAtualizado.Valor = produtoViewModel.Valor;
+            produtoAtualizado.Ativo = produtoViewModel.Ativo;
+            produtoAtualizado.FornecedorId = produtoViewModel.FornecedorId;
+            produtoAtualizado.Fornecedor = produtoViewModel.Fornecedor;
+
+            await _produtoService.Atualizar(_mapper.Map<Produto>(produtoAtualizado));
+
+            return RedirectToAction("Index");
         }
-        
+
         [Route("excluir-produto/{id:guid}")]
         [HttpGet]
         public async Task<ActionResult> Delete(Guid id)
@@ -147,6 +171,26 @@ namespace DevIO.AppMvc.Controllers
             produto.Fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
 
             return produto;
+        }
+
+        private bool UploadImagem(HttpPostedFileBase img, string imgPrefixo)
+        {
+            if (img == null || img.ContentLength <= 0)
+            {
+                ModelState.AddModelError(string.Empty, "Imagem em formato inválido!");
+                return false;
+            }
+
+            var path= Path.Combine(HttpContext.Server.MapPath("~/Imagens"), imgPrefixo + img.FileName);
+
+            if (System.IO.File.Exists(path))
+            {
+                ModelState.AddModelError(string.Empty, "Já existe um arquivo com este nome!");
+                return false;
+            }
+
+            img.SaveAs(path);
+            return true;
         }
 
         protected override void Dispose(bool disposing)
